4c07ed3cc6903b6d5e46f48c7955632223afc1a4
SUHEY <operato@naver.com>
2019-03-19 23:18:54 +0900

    first inti

---
+++ app/src/main/java/com/pnuproject/travellog/Main/MainActivity/Controller/MainActivity.java
@@ -0,0 +1,15 @@
+package com.pnuproject.travellog.Main.MainActivity.Controller;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.pnuproject.travellog.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/com/pnuproject/travellog/WelcomeLoading/WelcomeLoading.java
@@ -0,0 +1,35 @@
+package com.pnuproject.travellog.WelcomeLoading;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.pnuproject.travellog.Main.MainActivity.Controller.MainActivity;
+import com.pnuproject.travellog.R;
+import java.util.Timer;
+import java.util.TimerTask;
+
+
+public class WelcomeLoading extends AppCompatActivity {
+    private TimerTask timerTask;
+    private Timer timer;
+    final long WAIT_TIME_MILLISECOND = 2000; // 2 sec
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome_loading);
+
+        final Intent intent = new Intent(this, MainActivity.class);
+        timerTask = new TimerTask() {
+            @Override public void run() {
+                startActivity(intent);
+                finish();
+            }
+        };
+
+        timer = new Timer();
+        timer.schedule(timerTask, WAIT_TIME_MILLISECOND);
+
+    }
+}
