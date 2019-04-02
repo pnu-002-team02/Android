70f8937391c202afe749a4b9cc5f72b6d8ed40c1
youngwoo0411@naver.com <youngwoo0411@naver.com>
2019-04-02 16:41:15 +0900

    지도 뷰 생성 완료, 앱 종료 설정

--- app/src/main/java/com/pnuproject/travellog/Main/MainActivity/Controller/MainActivity.java
+++ app/src/main/java/com/pnuproject/travellog/Main/MainActivity/Controller/MainActivity.java
@@ -1,30 +1,69 @@
 package com.pnuproject.travellog.Main.MainActivity.Controller;
 
+import android.Manifest;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
+import android.os.Build;
 import android.support.design.widget.TabLayout;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.pnuproject.travellog.Main.HomeFragment.Controller.HomeFragment;
 import com.pnuproject.travellog.Main.MapFragment.Controller.MapFragment;
 import com.pnuproject.travellog.Main.MypageFragment.Controller.MypageFragment;
 import com.pnuproject.travellog.R;
+import com.pnuproject.travellog.etc.BackPressCloseHandler;
 import com.pnuproject.travellog.etc.SwipeViewPager;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.Vector;
 
 public class MainActivity extends AppCompatActivity {
     final public int MAX_TAB_PAGES = 3;
     Vector<Fragment> vFragments;
 
+    private BackPressCloseHandler backPressCloseHandler;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // 카카오 key hash 획득
+        try {
+            PackageInfo info = getPackageManager().getPackageInfo(getPackageName(), PackageManager.GET_SIGNATURES);
+            for (Signature signature : info.signatures) {
+                MessageDigest md = MessageDigest.getInstance("SHA");
+                md.update(signature.toByteArray());
+                Log.e("MY KEY HASH:", Base64.encodeToString(md.digest(), Base64.DEFAULT));
+            }
+        } catch (PackageManager.NameNotFoundException e) {
+
+        } catch (NoSuchAlgorithmException e) {
+
+        }
+
+        //뒤로가기 2번 누를 때 앱 종료
+        backPressCloseHandler = new BackPressCloseHandler(this);
+
+        //위치 permission 획득
+        if ( Build.VERSION.SDK_INT >= 23 &&
+                ContextCompat.checkSelfPermission( this, android.Manifest.permission.ACCESS_FINE_LOCATION ) != PackageManager.PERMISSION_GRANTED ) {
+            ActivityCompat.requestPermissions( this, new String[] { Manifest.permission.ACCESS_FINE_LOCATION  }, 1 );
+        }
+
 
         vFragments = new Vector<Fragment>();
 
@@ -84,4 +123,29 @@ public class MainActivity extends AppCompatActivity {
         });
 
     }
+
+
+    @Override
+    public void onBackPressed() {
+        backPressCloseHandler.onBackPressed();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        switch (requestCode) {
+            case 1:
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                } else {
+                    Toast toast=Toast.makeText(this,"현재 위치 기능 사용을 위한 권한 동의가 필요합니다.", Toast.LENGTH_SHORT);
+                    toast.show();
+                }
+                break;
+            default:
+                break;
+        }
+    }
 }
+
+
+
--- app/src/main/java/com/pnuproject/travellog/Main/MapFragment/Controller/MapFragment.java
+++ app/src/main/java/com/pnuproject/travellog/Main/MapFragment/Controller/MapFragment.java
@@ -1,15 +1,26 @@
 package com.pnuproject.travellog.Main.MapFragment.Controller;
 
+/**
+ * Created by s0woo on 2019-04-02.
+ */
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
 import com.pnuproject.travellog.R;
 
-public class MapFragment extends Fragment {
+import net.daum.mf.map.api.MapLayout;
+import net.daum.mf.map.api.MapPoint;
+import net.daum.mf.map.api.MapView;
+
+public class MapFragment extends Fragment
+        implements MapView.OpenAPIKeyAuthenticationResultListener, MapView.MapViewEventListener {
+
+    private MapView mMapView;
+
     public MapFragment() {
     }
 
@@ -18,4 +29,70 @@ public class MapFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_map, container, false);
     }
 
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        MapLayout mapLayout = new MapLayout(getActivity());
+        mMapView = mapLayout.getMapView();
+
+        mMapView.setDaumMapApiKey("e617e56195858d0ef3bf81ade21b855c");
+        mMapView.setOpenAPIKeyAuthenticationResultListener(this);
+        mMapView.setMapViewEventListener(this);
+        mMapView.setMapType(MapView.MapType.Standard);
+
+        ViewGroup mapViewContainer = (ViewGroup) getView().findViewById(R.id.map_view);
+        mapViewContainer.addView(mapLayout);
+    }
+
+    @Override
+    public void onMapViewInitialized(MapView mapView) {
+        //
+        mapView.setMapCenterPointAndZoomLevel(MapPoint.mapPointWithGeoCoord(35.231005,129.0800193), 2, true);
+    }
+
+    @Override
+    public void onMapViewCenterPointMoved(MapView mapView, MapPoint mapPoint) {
+
+    }
+
+    @Override
+    public void onMapViewZoomLevelChanged(MapView mapView, int i) {
+
+    }
+
+    @Override
+    public void onMapViewSingleTapped(MapView mapView, MapPoint mapPoint) {
+
+    }
+
+    @Override
+    public void onMapViewDoubleTapped(MapView mapView, MapPoint mapPoint) {
+
+    }
+
+    @Override
+    public void onMapViewLongPressed(MapView mapView, MapPoint mapPoint) {
+
+    }
+
+    @Override
+    public void onMapViewDragStarted(MapView mapView, MapPoint mapPoint) {
+
+    }
+
+    @Override
+    public void onMapViewDragEnded(MapView mapView, MapPoint mapPoint) {
+
+    }
+
+    @Override
+    public void onMapViewMoveFinished(MapView mapView, MapPoint mapPoint) {
+
+    }
+
+    @Override
+    public void onDaumMapOpenAPIKeyAuthenticationResult(MapView mapView, int i, String s) {
+
+    }
 }
