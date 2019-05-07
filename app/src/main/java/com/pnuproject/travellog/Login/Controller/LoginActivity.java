4806f25a22227ac891284181c44e27b28f8d745e
suhey <operato@naver.com>
2019-05-07 17:58:03 +0900

    클라이언트-서버통신 ID조회, 회원가입 추가

--- app/src/main/java/com/pnuproject/travellog/Login/Controller/LoginActivity.java
+++ app/src/main/java/com/pnuproject/travellog/Login/Controller/LoginActivity.java
@@ -7,10 +7,8 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.toolbox.Volley;
 import com.kakao.auth.ISessionCallback;
 import com.kakao.auth.KakaoSDK;
 import com.kakao.auth.Session;
@@ -23,7 +21,7 @@ import com.pnuproject.travellog.Signup.Controller.SignupActivity;
 import org.json.JSONObject;
 
 public class LoginActivity extends Activity {
-    private SessionCallback callback;
+    private ISessionCallback callback;
 
     @Override
     protected void onCreate(final Bundle savedInstanceState) {
@@ -32,7 +30,7 @@ public class LoginActivity extends Activity {
 
         final EditText inputID = (EditText) findViewById(R.id.login_te_ID);
         final EditText inputPW = (EditText) findViewById(R.id.login_te_PW);
-        final Button btn_signup = (Button) findViewById(R.id.btn_goto_signup);
+        final LinearLayout btn_signup = (LinearLayout) findViewById(R.id.btn_goto_signup);
         final Button btn_login = (Button) findViewById(R.id.btn_login_ok);
 
         //회원가입 액티비티 이동
@@ -50,7 +48,7 @@ public class LoginActivity extends Activity {
             public void onClick(View view) {
                 final String usrID = inputID.getText().toString();
                 final String usrPW = inputPW.getText().toString();
-
+/*
                 Response.Listener<String> responseListener = new Response.Listener<String>() {
                     @Override
                     public void onResponse(String response) {
@@ -85,9 +83,9 @@ public class LoginActivity extends Activity {
                 LoginRequest loginRequest = new LoginRequest(usrID, usrPW, responseListener);
                 RequestQueue queue = Volley.newRequestQueue(LoginActivity.this);
                 queue.add(loginRequest);
+*/
             }
         });
-
         //카카오 연동 구현
         KakaoSDK.init(new GlobalApplication.KakaoSDKAdapter());
 
@@ -95,6 +93,7 @@ public class LoginActivity extends Activity {
         Session.getCurrentSession().addCallback(callback);
         Session.getCurrentSession().checkAndImplicitOpen();
     }
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (Session.getCurrentSession().handleActivityResult(requestCode, resultCode, data)) {
